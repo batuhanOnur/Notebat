cd95b63160db7c8820aca57e9980cbaa681054ec
batuhanOnur <batuhanonur29@gmail.com>
2022-09-22 17:23:33 +0300

    workspace form

--- src/components/forms/WorkSpaceForm.tsx
+++ src/components/forms/WorkSpaceForm.tsx
@@ -1,11 +1,47 @@
-import { TextInput } from '@mantine/core';
+import { Group,TextInput,Text } from '@mantine/core';
+import { Select, Button } from '@mantine/core';
+import { forwardRef } from 'react';
+import IconProvider from '../../icons/IconProvider';
+import { data } from '../../utils/datas/iconSelectData'
+
+
+interface ItemProps extends React.ComponentPropsWithoutRef<'div'> {
+    value: string;
+    label: string;
+  }
+
+const SelectItem = forwardRef<HTMLDivElement, ItemProps>(
+    ({ label, ...others }: ItemProps, ref) => (
+        <div ref={ref} {...others}>
+            <Group>
+                <IconProvider icon={label} />
+                <Text>{label}</Text>
+            </Group>
+        </div>
+    )
+)
 
 const WorkSpaceForm = () => {
     return(
         <div>
-            <div>
+            <Group>
+                <div>
+                    <TextInput placeholder="Work space name" label="Name" />
+                </div>
+
+                <div className="ml-4">
+                    <Select 
+                    placeholder="icon" 
+                    label="Icon" 
+                    data={data} 
+                    itemComponent={SelectItem}
+                    />
+                </div>
                 
-            </div>
+                <div className="mt-6 ml-4">
+                    <Button variant="outline">Create</Button>
+                </div>
+            </Group>
         </div>
     )
 }
---
+++ src/utils/datas/iconSelectData.js
@@ -0,0 +1,26 @@
+export const data = [
+    {
+        value: 'cookie',
+        label: 'cookie'
+    },
+    {
+        value: 'book',
+        label: 'book'
+    },
+    {
+        value: 'growth',
+        label: 'growth'
+    },
+    {
+        value: 'database',
+        label: 'database'
+    },
+    {
+        value: 'pencil',
+        label: 'pencil'
+    },
+    {
+        value: 'notebook',
+        label: 'notebook'
+    },
+]
